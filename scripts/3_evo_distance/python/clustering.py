8a1025a8d3a3e0baac82743c1a367c610a140ed5
Rayann <rayann.larbi@etu.univ-lyon1.fr>
2026-02-10 15:26:41 +0100

    Clustering script update - not yet implemented

--- scripts/3_evo_distance/python/clustering.py
+++ scripts/3_evo_distance/python/clustering.py
@@ -6,7 +6,6 @@ parser = argparse.ArgumentParser(description="Takes a distance matrix and genera
 
 parser.add_argument("-i", "--input", required=True, help="Path to the distance matrix")
 parser.add_argument("-o", "--output", required=True, help="Path for the output file")
-parser.add_argument("-t", "--threshold", required=False, default=0.17, help="Distance threshold under which two species are considered close")
 
 
 args = parser.parse_args()
@@ -14,8 +13,24 @@ args = parser.parse_args()
 species = []
 
 matrix = pd.read_csv(args.input, sep="\t")
+species = matrix.iloc[:,0].to_list()
+matrix = matrix.iloc[:, 1:-1]
 
-species = matrix.iloc[0]
 
-matrix = matrix.iloc[1:, 1:]
+clusterer = hdbscan.HDBSCAN(metric="precomputed", min_cluster_size=3, min_samples=1, cluster_selection_epsilon = .1)
 
+clusterer.fit(matrix)
+
+with open(args.output, "w") as writer:
+    for i in range(len(clusterer.labels_)):
+        species[i] = [species[i], clusterer.labels_[i]]
+    ordered_labels = set(clusterer.labels_)
+
+    for label in ordered_labels:
+        writer.write("Cluster " + str(label) + ": ")
+        for i in range(len(species)):
+            if species[i][1] == label:
+                writer.write(species[i][0] + " ")
+        writer.write("\n")
+    writer.write(str(clusterer.labels_) + "\n")
+    writer.write(str(clusterer) + "\n")
