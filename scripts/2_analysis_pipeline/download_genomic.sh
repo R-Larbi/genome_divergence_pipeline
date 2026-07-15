dec5abafc4f3f44917968a211250f24fccd95bf0
Rayann <rayann.larbi@etu.univ-lyon1.fr>
2026-07-15 11:33:47 +0200

    Removal of obsolete step

--- scripts/2_analysis_pipeline/download_genomic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for acc in /banques/EvoDrivers/assemblies/*;
-do
-    wget -q -i /banques/EvoDrivers/assemblies/"$(basename '$acc')"/url_genomic.fna.txt -O /banques/EvoDrivers/assemblies/"$(basename '$acc')"/genomic.fna.gz
-done
